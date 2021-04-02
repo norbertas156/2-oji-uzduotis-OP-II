5d019daaa6c3e711e0b41321d81842b5ba4fffaf
dv1veidis <72011956+dv1veidis@users.noreply.github.com>
2021-04-02 16:26:56 +0300

    Add files via upload

--- Funkcijos/Benchmark.cpp
+++ Funkcijos/Benchmark.cpp
@@ -123,54 +123,60 @@ void benchmark (int pasirinkimas){
 string file;
 file="studentai1000";
 if(pasirinkimas == 1){
-    auto start = std::chrono::high_resolution_clock::now(); auto st=start;
     vector<Studentas> studentai;
     vector<Studentas> kietekai;
     vector<Studentas> nelaimingieji;
     for(int i=0; i<5; i++){
+    auto start = std::chrono::high_resolution_clock::now(); auto st=start;
     BenchmarkIvedimas(studentai, file);
     filtrasVector(studentai,kietekai, nelaimingieji, file);
     FiltroIsvedimas(kietekai, nelaimingieji, file);
     file=file+"0";
+    if(i!=4){
     studentai.clear();
     kietekai.clear();
     nelaimingieji.clear();
+    }
     std::chrono::duration<double> diff = std::chrono::high_resolution_clock::now()-start;
     cout<<"Programos trukme: "<<diff.count()<<endl;
     cout<<""<<endl; 
     }  
 }
-else if(pasirinkimas == 2){
-    auto start = std::chrono::high_resolution_clock::now(); auto st=start;
+else if(pasirinkimas == 2){ 
     deque<Studentas> studentai;
     deque<Studentas> kietekai;
     deque<Studentas> nelaimingieji;
     for(int i=0; i<5; i++){
+    auto start = std::chrono::high_resolution_clock::now(); auto st=start;
     BenchmarkIvedimas(studentai, file);
     filtrasDeque(studentai,kietekai, nelaimingieji, file);
     FiltroIsvedimas(kietekai, nelaimingieji, file);
     file=file+"0";
+    if(i!=4){
     studentai.clear();
     kietekai.clear();
     nelaimingieji.clear();
+    }
     std::chrono::duration<double> diff = std::chrono::high_resolution_clock::now()-start;
     cout<<"Programos trukme: "<<diff.count()<<endl;
     cout<<""<<endl; 
     }
 }
 else {
-    auto start = std::chrono::high_resolution_clock::now(); auto st=start;
     list<Studentas> studentai;
     list<Studentas> kietekai;
     list<Studentas> nelaimingieji; 
     for(int i=0; i<5; i++){
+    auto start = std::chrono::high_resolution_clock::now(); auto st=start;
     BenchmarkIvedimas(studentai, file);
     filtrasList(studentai,kietekai, nelaimingieji, file);
     ListIsvedimas(kietekai, nelaimingieji, file);
     file=file+"0";
+    if(i!=4){
     studentai.clear();
     kietekai.clear();
     nelaimingieji.clear();
+    }
     std::chrono::duration<double> diff = std::chrono::high_resolution_clock::now()-start;
     cout<<"Programos trukme: "<<diff.count()<<" s"<<endl;
     cout<<""<<endl;
