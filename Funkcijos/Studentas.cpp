e41c830239e2829a5496279fa93438a039385253
dv1veidis <72011956+dv1veidis@users.noreply.github.com>
2021-05-06 14:53:06 +0300

    Added destructor, copy and assignment operators

--- Funkcijos/Studentas.cpp
+++ Funkcijos/Studentas.cpp
@@ -1,12 +1,34 @@
 #include "My_lib.h"
 #include "Studentas.h"
 
+Studentas::~Studentas()= default;
+
+Studentas::Studentas(const Studentas& studentas){
+    vardas_=studentas.vardas_;
+    pavarde_=studentas.pavarde_;
+    vidurkis_=studentas.vidurkis_;
+    egzaminas_=studentas.egzaminas_;
+    mediana_=studentas.mediana_;
+    pazymiai_=studentas.pazymiai_;
+}
+
+Studentas &Studentas::operator=(const Studentas &studentas){
+
+if (&studentas == this) return *this;
+
+vardas_=studentas.vardas_;
+pavarde_=studentas.pavarde_;
+vidurkis_=studentas.vidurkis_;
+egzaminas_=studentas.egzaminas_;
+mediana_=studentas.mediana_;
+pazymiai_=studentas.pazymiai_;
+return *this;
+}
 
 Studentas::Studentas(const string &vardas, const string &pavarde){
     vardas_=vardas;
     pavarde_=pavarde;
 }
-
 
 void Studentas::setPazymiai(int pazymys){
     pazymiai_.push_back(pazymys);
